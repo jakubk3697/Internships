c30009b91cdbaf63fa49d7ede027de8d5a1e2d29
jakubk3697 <jakubkaminski3697@gmail.com>
2023-03-03 13:12:56 +0100

    Handling styles in NextJS - global.css, utils modules and layout module

--- 1. NextJS docs/pages/_app.js
+++ 1. NextJS docs/pages/_app.js
@@ -5,6 +5,8 @@
 //  - Add global metadata
 //  - Add global analytics
 
+import '../styles/global.css';
+
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />;
 }
--- 1. NextJS docs/pages/index.js
+++ 1. NextJS docs/pages/index.js
@@ -1,72 +1,23 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
+import Layout from '../components/layout';
+import { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
 
 export default function Home() {
   return (
-    <>
+    <Layout home>
       <Head>
-        <title>Custom title</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{siteTitle}</title>
       </Head>
-      <div className={styles.container}>
-        <h1 className="title">
-          Read <Link href="/posts/first-post">this page!</Link>
-        </h1>
-      </div>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </>
-
-
-  )
+      <section className={utilStyles.headingMd}>
+        <p>Hello, I'm Kuba. I'm beginning software engineer who will use NextJS with ♥️ </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
 }
