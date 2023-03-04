66962dacb23a297e7ee139402fc824c1afee5c77
jakubk3697 <jakubkaminski3697@gmail.com>
2023-03-04 15:43:54 +0100

    Cat API Mockup using nextjs works

--- 1. NextJS docs/pages/api/hello.js
+++ 1. NextJS docs/pages/api/hello.js
@@ -1,3 +1,10 @@
-export default function handler(req, res) {
-    res.status(200).json({ text: 'Hello', error: false });
+import { fetchCatFacts } from '../../lib/fetchCatFacts'
+
+export default async function handler(req, res) {
+    try {
+        const result = await fetchCatFacts()
+        res.status(200).send({ result })
+    } catch (err) {
+        res.status(500).send({ error: 'failed to fetch data' })
+    }
 }
--- 1. NextJS docs/pages/api/user.js
+++ 1. NextJS docs/pages/api/user.js
@@ -1,3 +1,4 @@
 export default function handler(req, res) {
+    if (req.method !== 'GET') return;
     res.status(200).json({ name: 'Jakub', surName: 'Kaminski', age: 25, department: 'IT' });
 }
