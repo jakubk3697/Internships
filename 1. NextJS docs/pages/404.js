6e728e94e966820a57b4cfeef76ce60b7abe0e76
jakubk3697 <jakubkaminski3697@gmail.com>
2023-03-08 12:54:51 +0100

    Deleted Next JS folder with docs examples to keep repo clean

--- 1. NextJS docs/pages/404.js
+++
@@ -1,4 +0,0 @@
-// This component is used to create a custom 404 page for NextJS. It is used to override the default 404 page.
-export default function Custom404() {
-    return <h1>404 - Page Not Found</h1>;
-}
